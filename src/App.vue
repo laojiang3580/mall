c94fad06d890a275882d56b7d4b963c375028b3b
jiangzibo <bo154003163@qq.com>
2021-10-12 21:48:11 +0800

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="nav">
+  <div>
+    <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  </div>
 </template>
 
 <style>
